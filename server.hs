5a7053451a56cb2f2b0c9ab33f831f748dbed96f
philopon <philopon.dependence@gmail.com>
2014-06-15 04:04:48 +0900

    add timeout option.

--- server.hs
+++ server.hs
@@ -39,10 +39,11 @@ newtype Timeout = Timeout Unique deriving (Eq, Typeable)
 instance Show Timeout where show _ = "<<timeout>>"
 instance Exception Timeout
 
-data Options = Options { optJisyo :: FilePath
-                       , optPort  :: Word16
-                       , optCode  :: String
-                       , optPlist :: Bool
+data Options = Options { optJisyo   :: FilePath
+                       , optPort    :: Word16
+                       , optCode    :: String
+                       , optPlist   :: Bool
+                       , optTimeout :: Int
                        }
 
 main :: IO ()
@@ -50,7 +51,7 @@ main = do
     homef <- maybe id (\f -> (f </>)) <$> lookupEnv "HOME"
     execParser (opts homef) >>= \case
         Options{optPlist = True} -> writePlist
-        Options{..} -> doAction optJisyo optPort optCode
+        Options{..} -> doAction optTimeout optJisyo optPort optCode
   where
     opts homef = info (helper <*> options homef) fullDesc
     defOpts s  = long (map toLower s) <> short (head s) <> metavar (map toUpper s)
@@ -59,9 +60,10 @@ main = do
         <*> option    (defOpts "port" <> value 1178 <> help "port" <> showDefault)
         <*> strOption (defOpts "code" <> value "EUC-JP" <> help "charcode" <> showDefaultWith id)
         <*> switch    (long   "plist" <> help "write plist of launchd")
+        <*> option    (defOpts "timeout" <> value 200000 <> help "google ime request timeout" <> showDefault)
 
-doAction :: FilePath -> Word16 -> String -> IO ()
-doAction jisyo usePort charCode =
+doAction :: Int -> FilePath -> Word16 -> String -> IO ()
+doAction to jisyo usePort charCode =
     SQLite.withConnection jisyo $ \conn -> 
     withManager defaultManagerSettings $ \mgr ->
     withSocketsDo $ 
@@ -69,7 +71,7 @@ doAction jisyo usePort charCode =
         icu <- ICU.open charCode Nothing
         forever ( do
             mbmsg <- recv sock 10240
-            maybe (throwIO End) (talk icu mgr conn sock . toKey icu) mbmsg
+            maybe (throwIO End) (talk to icu mgr conn sock . toKey icu) mbmsg
                 `catches` (handler icu sock mbmsg)
             ) `catch` (\End -> return ())
   where
@@ -82,13 +84,13 @@ doAction jisyo usePort charCode =
                 send sock (ICU.fromUnicode icu $ '0' `T.cons` toKey icu msg `T.append` " \n")
         ]
 
-talk :: ICU.Converter -> Manager -> SQLite.Connection -> Socket -> T.Text -> IO ()
-talk icu mgr conn sock key
+talk :: Int -> ICU.Converter -> Manager -> SQLite.Connection -> Socket -> T.Text -> IO ()
+talk to icu mgr conn sock key
     | T.head key == '0' = throwIO End
-    | T.head key == '1' = lookupCandidates False icu conn (T.tail key) mgr >>= send sock
+    | T.head key == '1' = lookupCandidates to False icu conn (T.tail key) mgr >>= send sock
     | T.head key == '2' = send sock "0.1 \n"
     | T.head key == '3' = send sock "127.0.0.1:0.0.0.0: \n"
-    | T.head key == '4' = lookupCandidates True icu conn (T.tail key) mgr >>= send sock
+    | T.head key == '4' = lookupCandidates to True icu conn (T.tail key) mgr >>= send sock
     | otherwise         = return ()
 
 formatValues :: T.Text -> [(T.Text, Maybe T.Text)] -> T.Text
@@ -96,12 +98,12 @@ formatValues k [] = '4' `T.cons` k `T.append` " \n"
 formatValues _ vs = (`T.append` "/\n") . ("1/" `T.append`) . T.intercalate "/" $
     map (\(v,a) -> v `T.append` maybe T.empty (T.cons ';') a) vs
 
-lookupCandidates :: Bool -> ICU.Converter -> Connection -> T.Text -> Manager -> IO S.ByteString
-lookupCandidates cand icu conn k mgr = SQLite.withTransaction conn $ do
+lookupCandidates :: Int -> Bool -> ICU.Converter -> Connection -> T.Text -> Manager -> IO S.ByteString
+lookupCandidates to cand icu conn k mgr = SQLite.withTransaction conn $ do
     i <- fromOnly . head <$> SQLite.query conn "SELECT count(*) FROM googleime WHERE key = ? LIMIT 1" (Only k)
     if (i :: Int) > 0
         then return ()
-        else timeout (2 * 10 ^ (5 :: Int)) () (requestGoogleIme k mgr >>= 
+        else timeout to () (requestGoogleIme k mgr >>= 
             mapM_ (\r -> do
                 SQLite.execute conn "INSERT OR IGNORE INTO dict (jisyo,key,value,priority) VALUES ('googleime', ?, ?, 1000)" (k,r)
                 SQLite.execute conn "INSERT OR IGNORE INTO googleime VALUES (?)" (Only k)
